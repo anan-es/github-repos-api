d85290a9fdd51b877a48ad7114388337f84cb005
Jakub <anana5@post.cz>
2024-08-08 20:17:44 +0200

    Initial project structure

---
+++ src/main/java/me/jakub/githubreposapi/GithubReposApiApplication.java
@@ -0,0 +1,13 @@
+package me.jakub.githubreposapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubReposApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GithubReposApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/jakub/githubreposapi/GithubReposApiApplicationTests.java
@@ -0,0 +1,13 @@
+package me.jakub.githubreposapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubReposApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
