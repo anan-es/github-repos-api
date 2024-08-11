80dce96ade88b3c7055d74cea0447cb6dd2c0452
Jakub <anana5@post.cz>
2024-08-11 18:03:11 +0200

    Mark JSON properties in Repository and Owner models

--- src/main/java/me/jakub/githubreposapi/github/model/Owner.java
+++ src/main/java/me/jakub/githubreposapi/github/model/Owner.java
@@ -1,10 +1,14 @@
 package me.jakub.githubreposapi.github.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class Owner {
+    @JsonProperty
     private String login;
+    @JsonProperty
     private Integer id;
+    @JsonProperty
     private String url;
 }
--- src/main/java/me/jakub/githubreposapi/github/model/Repository.java
+++ src/main/java/me/jakub/githubreposapi/github/model/Repository.java
@@ -2,13 +2,20 @@ package me.jakub.githubreposapi.github.model;
 
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class Repository {
+    @JsonProperty
     private Integer id;
+    @JsonProperty
     private String nodeId;
+    @JsonProperty
     private String name;
+    @JsonProperty
     private Owner owner;
+    @JsonProperty
     private Boolean fork;
+    @JsonProperty
     private String url;
 }
